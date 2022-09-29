8959448464614a23f151394d111f5ce37a8e5081
FloLcs <florianlcs16@gmail.com>
2022-09-29 12:25:01 +0200

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,46 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
 });
+
+// SWIPER JS
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.5,
+  spaceBetween: 20,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  breakpoints: {
+    425: {
+      slidesPerView: 1.8,
+    },
+    500: {
+      slidesPerView: 2.1,
+    },
+    600: {
+      slidesPerView: 2.4,
+    },
+    768: {
+      slidesPerView: 2.8,
+    },
+    900: {
+      slidesPerView: 3.3,
+    },
+    1000: {
+      slidesPerView: 3.7,
+    },
+    1000: {
+      slidesPerView: 3.4,
+    },
+    1500: {
+      slidesPerView: 3.8,
+    },
+    2000: {
+      slidesPerView: 4.2,
+    },
+    2500: {
+      slidesPerView: 4.6,
+    },
+    3000: {
+      slidesPerView: 5.2,
+    },
+  },
+});
